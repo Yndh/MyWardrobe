5e4d7a476d025d1c058cc73c5e5c968917ae3923
Yndh <przybylski-adrian@o2.pl>
2024-05-04 15:02:23 +0200

    Add Navigation Menu

--- app/src/main/java/com/example/mywardrobe/MainActivity.kt
+++ app/src/main/java/com/example/mywardrobe/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.mywardrobe
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var navigation: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,27 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        navigation = findViewById(R.id.navigationView)
+        navigation.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.action_home -> {
+                    Toast.makeText(this@MainActivity, "Home", Toast.LENGTH_LONG ).show()
+                    true
+                }
+                R.id.action_create -> {
+                    Toast.makeText(this@MainActivity, "Create", Toast.LENGTH_LONG ).show()
+                    true
+                }
+                R.id.action_wardrobe -> {
+                    Toast.makeText(this@MainActivity, "Wardrobe", Toast.LENGTH_LONG ).show()
+                    true
+                }
+                else -> false
+            }
+        }
     }
+
+
+
 }
